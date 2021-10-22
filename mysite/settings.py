5065653b5c623745a551c5f0cf24703512db0f41
Scott Canning <sc7979@nyu.edu>
2021-10-22 14:10:01 -0400

    updated ci

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y2#z$et837l0(v@+o1p_@j2%15e$3xiwjaza*n8+4&y)2s61ol
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]  #'scott-cannings-swe1-app.herokuapp.com'
+ALLOWED_HOSTS = ["*"] 
 
 
 # Application definition
@@ -86,7 +86,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": 
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
